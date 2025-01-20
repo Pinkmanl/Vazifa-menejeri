450b9d93175f5d54ccef50b8c3354bfc6740164c
Pinkmanl <abduvohod23@gmail.com>
2025-01-20 11:05:57 +0500

    upgraded

--- vazifaMenejeri/Program.cs
+++ vazifaMenejeri/Program.cs
@@ -1,66 +1,76 @@
-﻿using System;
+using System;
 namespace vazifaMenejeri
 {
-    class vazifaMenejeri
+    class VazifaMenejeri
     {
         static void Main()
         {
             int cmd = 0;
-            string[] vazifalar = ["ertalab turish", "ovqat yeyish"];
-
+            string[] tasks = ["ertalab turish", "ovqat yeyish"];
+            PrintTasks(tasks);
             do
             {
-                Console.WriteLine("Tanlovni kiriting");
-                Console.WriteLine("1.Vazifa qo'shish");
-                Console.WriteLine("2. Vazifani o'chirish");
-                Console.WriteLine("3. Vazifaning bajarilganligini belgilash");
-                Console.WriteLine("4. Vazifalar ro'yxatini ko'rish");
-                Console.WriteLine("5. Dasturdan chiqish");
-                Console.Write("Tanlovni kiriting: ");
+                Console.WriteLine("|--------------------------------------------|");
+                Console.WriteLine("| Amallar                                    |");
+                Console.WriteLine("| 1. Vazifa qo'shish                         |");
+                Console.WriteLine("| 2. Vazifani o'chirish                      |");
+                Console.WriteLine("| 3. Vazifaning bajarilganligini belgilash   |");
+                Console.WriteLine("| 4. Vazifalar ro'yxatini ko'rish            |");
+                Console.WriteLine("| 5. Dasturdan chiqish                       |");
+                Console.WriteLine("|--------------------------------------------|");
+                Console.Write("Tanlovni kiriting:         ");
                 cmd = Convert.ToInt32(Console.ReadLine() ?? "");
                 switch (cmd)
                 {
 
                     //1.Vazifa qo'shish
                     case 1:
-                        System.Console.WriteLine("qo'shadigan vazifani kiriting");
-                        string qoshimchaVazifa = Console.ReadLine() ?? "";
-                        Array.Resize(ref vazifalar, vazifalar.Length + 1);
-                        vazifalar[vazifalar.Length - 1] = qoshimchaVazifa;
-
+                        System.Console.WriteLine("Yangi vazifa nomini kiriting:");
+                        string newTask = Console.ReadLine() ?? "";
+                        Array.Resize(ref tasks, tasks.Length + 1);
+                        tasks[tasks.Length - 1] = newTask;
+                        PrintTasks(tasks);
                         break;
+
                     //2.Vazifani o'chirish
                     case 2:
-                        Console.WriteLine("Natija: " + string.Join(", ", vazifalar));
+                        Console.WriteLine("Natija: " + string.Join(", ", tasks));
                         System.Console.WriteLine("qaysi vazifani o'chirmoqchisiz");
                         int m = Convert.ToInt32(Console.ReadLine() ?? "");
-                        int turnOff = m - 1;
-                        Array.Clear(vazifalar, turnOff, 1);
+                        int deleteIndex = m - 1;
+                        Array.Clear(tasks, deleteIndex, 1);
+                        PrintTasks(tasks);
                         break;
                     //3.Vazifaning bajarilganligini belgilash
                     case 3:
                         System.Console.WriteLine("qaysi vazifani bajarildiga o'zgartirmoqchisiz");
                         int done = Convert.ToInt32(Console.ReadLine() ?? "");
-                        int minus = done - 1;
-                        string bajarildi = "  bajarildi";
-                        int size = vazifalar.Length;
-                        for (int i = 0; i <= size; i++)
-                        {
-                            if (minus == i)
-                               vazifalar[i]= vazifalar[i] + bajarildi;
-                        }
+                        int index = done - 1;
+                        string completed = " :bajarildi";
+                        tasks[index] = tasks[index].EndsWith(completed)
+                                    ? tasks[index]
+                                    : $"{tasks[index]} {completed}";
+                        PrintTasks(tasks);
                         break;
                     //4.Vazifalar ro'yxatini ko'rish
                     case 4:
-                        Console.WriteLine("Natija: " + string.Join(", ", vazifalar));
+                        PrintTasks(tasks);
                         System.Console.WriteLine();
                         break;
                 }
 
             } while (cmd != 5);
 
-            Console.WriteLine("Natija: " + string.Join(", ", vazifalar));
+            PrintTasks(tasks);
 
+        }
+        private static void PrintTasks(string[] tasks)
+        {
+            System.Console.WriteLine("Vazifalar ro'yxati:");
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                System.Console.WriteLine($" >> {i + 1}. {tasks[i]}");
+            }
         }
     }
 }
